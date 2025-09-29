72dcd1a75aca8cedd9a697e03b206e10d4eecbec
aby639 <sunnyvarghese25007@gmail.com>
2025-09-29 13:16:13 +0530

    chore(frontend): static export + ignore ESLint during build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                // write static files to /out
+  output: 'export',                 // write static files to /out
   eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true }    // needed for static export
+  images: { unoptimized: true }     // needed for static export
 };
 module.exports = nextConfig;
