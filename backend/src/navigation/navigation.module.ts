cbe24f775e29dbdd537abb06a067ba742d0c5d5b
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-09-29 18:51:13 +0530

    Update navigation.module.ts

--- backend/src/navigation/navigation.module.ts
+++ backend/src/navigation/navigation.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Navigation } from '../entities/navigation.entity';
 import { NavigationController } from './navigation.controller';
+import { NavigationService } from './navigation.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Navigation])],
   controllers: [NavigationController],
+  providers: [NavigationService],                 // <-- register it
+  exports: [TypeOrmModule, NavigationService],    // optional, handy elsewhere
 })
 export class NavigationModule {}
