3a1b37802537e3b8d00593f6387ed3596d432030
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-09-29 14:17:17 +0530

    Delete frontend/next.config.ts

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ['http://192.168.1.2:3000', 'http://localhost:3000'],
-  },
-};
-
-export default nextConfig;
