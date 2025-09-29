f31e1811fe23783cab7e0ca52ef91a17849790fc
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-09-29 23:10:17 +0530

    Update navigation.module.ts

--- backend/src/navigation/navigation.module.ts
+++ backend/src/navigation/navigation.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Navigation } from '../entities/navigation.entity';
+import { Category } from '../entities/category.entity';
+import { Product } from '../entities/product.entity';
+
 import { NavigationController } from './navigation.controller';
 import { NavigationService } from './navigation.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Navigation])],
+  // include all repos we need for seeding
+  imports: [TypeOrmModule.forFeature([Navigation, Category, Product])],
   controllers: [NavigationController],
-  providers: [NavigationService],                 // <-- register it
-  exports: [TypeOrmModule, NavigationService],    // optional, handy elsewhere
+  providers: [NavigationService],
+  exports: [TypeOrmModule, NavigationService],
 })
 export class NavigationModule {}
