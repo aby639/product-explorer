6780a6b4ed4b63d979a444e897e9248c1add462a
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-09-30 14:58:39 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // make sure you are NOT exporting statically
-  // output: 'export', // <-- keep this commented/removed
-
   eslint: { ignoreDuringBuilds: true },
-
   images: {
-    unoptimized: true, // fine on Render
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'images.worldofbooks.com' },
       { protocol: 'https', hostname: 'www.worldofbooks.com' },
-      // if you ever see covers coming from a different WOB CDN, add it here
-      // { protocol: 'https', hostname: 'cdn.worldofbooks.com' },
     ],
   },
-
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
+  outputFileTracingRoot: process.cwd(),
 };
-
 module.exports = nextConfig;
