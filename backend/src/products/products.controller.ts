617a0d5715482a88c21584373b811635c990ee73
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-10-03 00:02:07 +0530

    Update products.controller.ts

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -4,20 +4,20 @@ import { ListProductsQueryDto } from './dto/get-products.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly svc: ProductsService) {}
+  constructor(private readonly products: ProductsService) {}
 
   @Get()
   async list(@Query() q: ListProductsQueryDto) {
-    return this.svc.list(q);
+    return this.products.list(q);
   }
 
   @Get(':id')
   async detail(@Param('id') id: string) {
-    return this.svc.detail(id);
+    return this.products.detail(id);
   }
 
   @Post(':id/refresh')
   async refresh(@Param('id') id: string) {
-    return this.svc.refreshProduct(id);
+    return this.products.refresh(id);
   }
 }
