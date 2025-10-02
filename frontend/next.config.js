f767cc5cacf7bf36687a326ad9577030868d8189
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-10-03 00:32:26 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.worldofbooks.com' },
       { protocol: 'https', hostname: 'www.worldofbooks.com' },
-    ],
+      { protocol: 'https', hostname: 'static.wobcdn.com' },
+      { protocol: 'https', hostname: 'd1w7fb2mkkr3kw.cloudfront.net' }
+    ]
   },
-  outputFileTracingRoot: process.cwd(),
+  outputFileTracingRoot: process.cwd()
 };
 module.exports = nextConfig;
