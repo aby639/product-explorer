ae9eebc4c0e620caf0c7a88b0dcc442ff497227f
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-10-04 21:12:53 +0530

    Update create-view.dto.ts

--- backend/src/views/dto/create-view.dto.ts
+++ backend/src/views/dto/create-view.dto.ts
@@ -1,9 +1,15 @@
-import { IsArray, IsString } from 'class-validator';
+import { IsArray, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateViewDto {
   @IsString()
+  @MaxLength(120)
   sessionId!: string;
 
+  @IsString()
+  @MaxLength(1024)
+  path!: string;
+
+  @IsOptional()
   @IsArray()
-  pathJson!: string[];
+  trail?: string[];
 }
