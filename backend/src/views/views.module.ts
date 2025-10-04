d0dd4c95f6ca174c33e9949ffc8b45e82d1cf563
ABY DANIEL VARGHESE <sunnyvarghese25007@gmail.com>
2025-10-04 21:14:14 +0530

    Update views.module.ts

--- backend/src/views/views.module.ts
+++ backend/src/views/views.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ViewHistory } from '../entities/view-history.entity';
-import { ViewsService } from './views.service';
 import { ViewsController } from './views.controller';
+import { ViewsService } from './views.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ViewHistory])],
   controllers: [ViewsController],
   providers: [ViewsService],
+  exports: [],
 })
 export class ViewsModule {}
